7bf4ef82e9dac2a441db90450147f62a9d0826f8
davidbell <bello81@gmail.com>
2021-09-17 10:21:45 +1000

    adding in blockfrost, nowhere near complete

---
+++ BikeBlock/ViewModels/WalletDisplayViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BikeBlock.models;
+using System.IO;
+
+using CardanoSharp.Wallet;
+using CardanoSharp.Wallet.Enums;
+using CardanoSharp.Wallet.Models.Keys;
+using CardanoSharp.Wallet.Extensions.Models;
+using CardanoSharp.Wallet.Models;
+using CardanoSharp.Wallet.Models.Derivations;
+using CardanoSharp.Wallet.Models.Transactions;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text.Json;
+using BikeBlock.views;
+using Xamarin.Forms;
+using BikeBlock.Persistence;
+
+namespace BikeBlock.ViewModels
+{
+    public class WalletDisplayViewModel : BaseViewModel
+    {
+        
+        private IPageService _pageService;
+        private IWalletStore _walletStore;
+
+        public Wallet Wallet { get; set; } = new Wallet();
+        //public ICommand BuildWalletCommand
+        //{
+        //    get;
+        //    private set;
+        //}
+
+        public WalletDisplayViewModel(IPageService pageService, IWalletStore walletStore)
+        {
+            _pageService = pageService;
+            _walletStore = walletStore;
+           // BuildWalletCommand = new Command(async () => await buildWallet());
+        }
+
+       
+    }
+}
---
+++ BikeBlock/views/WalletDisplayPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BikeBlock.views
+{
+    public partial class WalletDisplayPage : ContentPage
+    {
+        public WalletDisplayPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
