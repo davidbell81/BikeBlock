70632e30bea5acfae13334ec4469f238b9d42389
davidbell <bello81@gmail.com>
2021-09-17 12:30:34 +1000

    adding in blockfrost, nowhere near complete

--- BikeBlock/models/Wallet.cs
+++ BikeBlock/models/Wallet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CardanoSharp.Wallet;
 using CardanoSharp.Wallet.Enums;
 using CardanoSharp.Wallet.Models.Keys;
@@ -7,25 +8,33 @@ namespace BikeBlock.models
 {
     public class Wallet
     {
-        [Ignore]
-        public Mnemonic Mnemonic { get; set; }
+        
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
         public String Name { get; set; }
 
+        public String SecureKeyBlob { get; set; }
+
         [Ignore]
         public String Password { get; set; }
 
-        public String SecureKeyBlob { get; set; }
+        [Ignore]
+        public Mnemonic Mnemonic { get; set; }
 
         [Ignore]
         public PrivateKey MasterKey { get; set; }
 
+        [Ignore]
+        public Dictionary<String, int> Contents { get; set; }
+
         public Wallet()
         {
         }
+
+        
+
 
         
     }
